921ad0e6b932ca713d93b9081d4597ea39fb3483
alex <aabinaei@gmail.com>
2023-02-13 21:30:34 -0800

    route53 init

--- deployment/route53.tf
+++ deployment/route53.tf
@@ -1,7 +1,7 @@
-// resource "aws_route53_zone" "main" {
-//   name = var.domain_name
-// }
-//
+resource "aws_route53_zone" "main" {
+  name = var.domain_name
+}
+
 // resource "aws_route53_record" "main" {
 //   zone_id = aws_route53_zone.main.zone_id
 //   name    = aws_route53_zone.main.name
@@ -24,15 +24,14 @@
 //     zone_id                = aws_cloudfront_distribution.www.hosted_zone_id
 //   }
 // }
-//
-// resource "aws_route53_record" "dev" {
-//   zone_id = aws_route53_zone.main.zone_id
-//   name    = "dev.${var.domain_name}"
-//   type    = "A"
-//   alias {
-//     evaluate_target_health = false
-//     name                   = "s3-website-${var.aws_region}.amazonaws.com"
-//     zone_id                = aws_s3_bucket.domain.hosted_zone_id
-//   }
-// }
-//
+
+resource "aws_route53_record" "dev" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "dev.${var.domain_name}"
+  type    = "A"
+  alias {
+    evaluate_target_health = false
+    name                   = "s3-website-${var.aws_region}.amazonaws.com"
+    zone_id                = aws_s3_bucket.domain.hosted_zone_id
+  }
+}
