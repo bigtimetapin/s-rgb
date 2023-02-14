23670d53d92b66308db517ca0912df920e3c2105
alex <aabinaei@gmail.com>
2023-02-13 21:53:49 -0800

    cloudfront

--- deployment/variables.tf
+++ deployment/variables.tf
@@ -3,7 +3,7 @@ variable "domain_name" {
   default     = "rgb.industries"
 }
 
-// variable "cert_arn" {
-//   description = "SSL Cert"
-//   default     = "arn:aws:acm:us-east-1:504084586672:certificate/767ceeb9-203c-4de4-9a53-d9790c09fd95"
-// }
+variable "cert_arn" {
+  description = "SSL Cert"
+  default     = "arn:aws:acm:us-east-1:072445992302:certificate/fa380625-f4a9-40e1-a18b-a9473182b29c"
+}
