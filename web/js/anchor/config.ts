54067c4c43af53c68d748e44ad2b9543ef33eb80
alex <aabinaei@gmail.com>
2023-02-28 19:25:32 -0800

    mainnet deploy

--- web/js/anchor/config.ts
+++ web/js/anchor/config.ts
@@ -1,7 +1,7 @@
 import {PublicKey} from "@solana/web3.js";
-// import {RPC_URL} from "./rpc";
+import {RPC_URL} from "./rpc";
 
 
 export const COMMITMENT = "processed";
 export const PROGRAM_ID = new PublicKey("3tgVL9TfPbEe7tgxjjmMCLUMzyFqspJkGW3u4hiEoFu6");
-export const NETWORK = "http://127.0.0.1:8899"; //RPC_URL
+export const NETWORK = RPC_URL
