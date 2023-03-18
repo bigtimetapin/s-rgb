0909f19b38334f6f44e3d84b3db6a5aeca8e4af7
alex <aabinaei@gmail.com>
2023-03-18 03:39:52 -0700

    bumped footer with deck

--- elm/src/View/Footer.elm
+++ elm/src/View/Footer.elm
@@ -8,37 +8,59 @@ import Msg.Msg exposing (Msg)
 view : Html Msg
 view =
     Html.footer
-        [ class "level has-right"
+        [ class "level mx-5 my-2"
         ]
         [ Html.div
-            [ class "level-item"
+            [ class "level-left"
             ]
-            [ Html.a
-                [ href "https://github.com/bigtimetapin/s-rgb"
-                , target "_blank"
+            [ Html.div
+                [ class "level-item"
                 ]
-                [ Html.span
+                [ Html.a
+                    [ href "https://github.com/bigtimetapin/s-rgb"
+                    , target "_blank"
+                    ]
+                    [ Html.span
+                        [ class "icon is-medium"
+                        ]
+                        [ Html.i
+                            [ class "fab fa-sm fa-github"
+                            ]
+                            []
+                        ]
+                    ]
+                ]
+            , Html.div
+                [ class "level-item"
+                ]
+                [ Html.a
                     [ class "icon is-medium"
+                    , href "mailto:hi@seangreen.info"
+                    , target "_blank"
                     ]
                     [ Html.i
-                        [ class "fab fa-sm fa-github"
+                        [ class "far fa-sm fa-envelope"
                         ]
                         []
                     ]
                 ]
             ]
         , Html.div
-            [ class "level-item"
+            [ class "level-right"
             ]
-            [ Html.a
-                [ class "icon is-medium"
-                , href "mailto:hi@seangreen.info"
-                , target "_blank"
+            [ Html.div
+                [ class "level-item is-text-container-5"
                 ]
-                [ Html.i
-                    [ class "far fa-sm fa-envelope"
+                [ Html.div
+                    [ class "is-size-5 has-text-weight-bold"
                     ]
-                    []
+                    [ Html.a
+                        [ href "https://docs.google.com/presentation/d/e/2PACX-1vTbYb_vM7uBODzRfu0xWA3xsFbI3oaOSkWXF9Z9ccTSC6c_Gz8bhw7K6Dy8amuNxKhQnDQBWm8hxB9Z/pub?start=false&slide=id.p"
+                        , target "_blank"
+                        ]
+                        [ Html.text "?"
+                        ]
+                    ]
                 ]
             ]
         ]
