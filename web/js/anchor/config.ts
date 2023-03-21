8ed7290fa675c6a03b8df16ea936e450e223ba1a
alex <aabinaei@gmail.com>
2023-03-20 20:08:15 -0700

    bumped paint-program id

--- web/js/anchor/config.ts
+++ web/js/anchor/config.ts
@@ -4,5 +4,5 @@ import {RPC_URL} from "./rpc";
 export const COMMITMENT = "processed";
 export const STAKING_PROGRAM_ID = new PublicKey("FAimDhcwrK9hikkkFnLx3FXs32ZBk2kw8kwzbLJH8AcV");
 export const CRAFTING_PROGRAM_ID = new PublicKey("G4Wm9es5xZoZLpyzoFHrtgM4Vmo84CenCSrCK6ZxDSmf");
-export const PAINTING_PROGRAM_ID = new PublicKey("BHaigrDd4Ei5ok5XnoNCxzGCGeK9T9RAQtyWKFphGnhi");
+export const PAINTING_PROGRAM_ID = new PublicKey("5bySibubtXmihMXgRsyJVjRfc13SHgf5fi6nXwoUwisW");
 export const NETWORK = RPC_URL
