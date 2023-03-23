e4774306cec5a022bbf7d3256c5011162a5bd50f
alex <aabinaei@gmail.com>
2023-03-23 02:20:11 -0700

    burn-pixels-seven rs impl

--- programs/s-rgb-paint/src/ix/paint/burn/mod.rs
+++ programs/s-rgb-paint/src/ix/paint/burn/mod.rs
@@ -9,6 +9,7 @@ pub mod three;
 pub mod four;
 pub mod five;
 pub mod six;
+pub mod seven;
 
 fn almost_burn<'info>(
     token_program: &Program<'info, Token>,
---
+++ programs/s-rgb-paint/src/ix/paint/burn/seven.rs
@@ -0,0 +1,61 @@
+use anchor_lang::prelude::*;
+use partial_application::partial;
+use crate::BurnPixelsSeven;
+use crate::ix::paint::burn::almost_burn;
+
+pub fn ix(ctx: Context<BurnPixelsSeven>) -> Result<()> {
+    // partially apply almost burn
+    let burn = partial!(almost_burn =>
+        &ctx.accounts.token_program,
+        &ctx.accounts.payer,
+        &ctx.accounts.proof,
+        _, _, _
+    );
+    // burn pixel one
+    burn(
+        &ctx.accounts.pixel_one_mint,
+        &ctx.accounts.pixel_one_mint_ata,
+        |p| &p.burned.plan.one,
+    )?;
+    // burn pixel two
+    burn(
+        &ctx.accounts.pixel_two_mint,
+        &ctx.accounts.pixel_two_mint_ata,
+        |p| &p.burned.plan.two,
+    )?;
+    // burn pixel three
+    burn(
+        &ctx.accounts.pixel_three_mint,
+        &ctx.accounts.pixel_three_mint_ata,
+        |p| &p.burned.plan.three,
+    )?;
+    // burn pixel four
+    burn(
+        &ctx.accounts.pixel_four_mint,
+        &ctx.accounts.pixel_four_mint_ata,
+        |p| &p.burned.plan.four,
+    )?;
+    // burn pixel five
+    burn(
+        &ctx.accounts.pixel_five_mint,
+        &ctx.accounts.pixel_five_mint_ata,
+        |p| &p.burned.plan.five,
+    )?;
+    // burn pixel six
+    burn(
+        &ctx.accounts.pixel_six_mint,
+        &ctx.accounts.pixel_six_mint_ata,
+        |p| &p.burned.plan.five,
+    )?;
+    // burn pixel seven
+    burn(
+        &ctx.accounts.pixel_seven_mint,
+        &ctx.accounts.pixel_seven_mint_ata,
+        |p| &p.burned.plan.five,
+    )?;
+    // proof
+    let proof = &mut ctx.accounts.proof;
+    assert_eq!(proof.arity, 7);
+    proof.burned.burned = true;
+    Ok(())
+}
